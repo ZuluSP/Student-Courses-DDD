1eff4be1298b2544bd505ed8e996a86ca12469a8
Carlos Zulueta <zulueta.demadariaga.carlos@gmail.com>
2023-06-04 23:28:22 +0200

    refactor: Add table courses_counter with the field in JSON.

--- src/mooc/main/resources/database/mooc.sql
+++ src/mooc/main/resources/database/mooc.sql
@@ -8,6 +8,16 @@ CREATE TABLE IF NOT EXISTS courses (
     DEFAULT CHARSET = utf8mb4
     COLLATE = utf8mb4_unicode_ci;
 
+CREATE TABLE IF NOT EXISTS courses_counter (
+                                               id               CHAR(36) NOT NULL,
+                                               total            INT      NOT NULL,
+                                               existing_courses JSON     NOT NULL,
+                                               PRIMARY KEY (id)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
+
 CREATE TABLE IF NOT EXISTS steps (
                                      id    CHAR(36)     NOT NULL,
                                      title VARCHAR(155) NOT NULL,
@@ -33,6 +43,18 @@ CREATE TABLE IF NOT EXISTS steps_videos (
                                             text TEXT         NOT NULL,
                                             PRIMARY KEY (id),
                                             CONSTRAINT fk_steps_video__step_id FOREIGN KEY (id) REFERENCES steps(id)
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS domain_events (
+                                             id           CHAR(36)     NOT NULL,
+                                             aggregate_id CHAR(36)     NOT NULL,
+                                             name         VARCHAR(255) NOT NULL,
+                                             body         JSON         NOT NULL,
+                                             occurred_on  TIMESTAMP    NOT NULL,
+                                             PRIMARY KEY (id)
 )
     ENGINE = InnoDB
     DEFAULT CHARSET = utf8mb4
