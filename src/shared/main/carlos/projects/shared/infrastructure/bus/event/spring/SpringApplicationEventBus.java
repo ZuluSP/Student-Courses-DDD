849e051eb1444fb1659641decb4e44d41d7e048a
Carlos Zulueta <zulueta.demadariaga.carlos@gmail.com>
2023-06-04 21:28:02 +0200

    feature: SpringApplicationEventBus contract to handle the events via Spring framework.

---
+++ src/shared/main/carlos/projects/shared/infrastructure/bus/event/spring/SpringApplicationEventBus.java
@@ -0,0 +1,31 @@
+package carlos.projects.shared.infrastructure.bus.event.spring;
+
+import carlos.projects.shared.domain.Service;
+import carlos.projects.shared.domain.bus.event.DomainEvent;
+import carlos.projects.shared.domain.bus.event.EventBus;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Primary;
+
+import java.util.List;
+
+@Primary
+@Service
+public class SpringApplicationEventBus implements EventBus {
+
+    // Event publisher by spring. Will take all the @EventListener tags.
+    private final ApplicationEventPublisher publisher;
+
+    public SpringApplicationEventBus(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public void publish(List<DomainEvent> events) {
+        events.forEach(this::publish);
+    }
+
+    // With the publishEvent spring knows how to publish the event.
+    private void publish(final DomainEvent event) {
+        this.publisher.publishEvent(event);
+    }
+}
